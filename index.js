e92e4d954de5e1914b1727c0878568e32909a04e
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-10 21:41:35 -0700

    message

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = { 
+    name:  " ",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, 
+key, value) {
+const newEmployee = { ...employee };
+newEmployee.Sam;
+return newEmployee;
+
+};
+  
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name)
+{
+    employee[name]= Sam;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee,name){
+delete employee[name];
+return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(){
+    const newEmployee= { ...employee };
+  newEmployee[name] = " ";
+
+  return newEmployee;
+}
+
+
